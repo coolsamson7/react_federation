ecd809f19d8742d7a12a7cb59bf701076a97fa57
Andreas Ernst <andreas.ernst7@gmail.com>
2025-12-12 18:34:00 +0100

    layout

--- libs/portal/src/widgets/editor/SlidingPanel.tsx
+++ libs/portal/src/widgets/editor/SlidingPanel.tsx
@@ -100,6 +100,8 @@ export const SlidingPanel: React.FC<SlidingPanelProps> = ({
           height: "100%",
           borderLeft: "none",
           borderRight: "none",
+          borderTop: "none",
+          borderBottom: "none",
         };
       case "right":
         return {
@@ -110,6 +112,8 @@ export const SlidingPanel: React.FC<SlidingPanelProps> = ({
           height: "100%",
           borderRight: "none",
           borderLeft: "none",
+          borderTop: "none",
+          borderBottom: "none",
         };
       case "bottom":
         return {
